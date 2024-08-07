e8c93623633e563f19fdb4e72b7bfc6faa53e7cd
Ahmet Onder Mogol <aomogol@gmail.com>
2024-08-07 23:55:36 +0300

    update

--- 7-8-2024--kmaster1/master.sh
+++ 7-8-2024--kmaster1/master.sh
@@ -6,10 +6,13 @@ set -e
 
 # master nodekurulumu
 
+sudo kubeadm config images pull
+
 # tmux
 # sudo kubeadm init --control-plane-endpoint=172.27.5.14 --node-name k8s-master --pod-network-cidr=10.244.0.0/16
 
-sudo kubeadm init --node-name kmaster --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init 
+# --node-name kmaster --pod-network-cidr=10.244.0.0/16
 
 
 mkdir -p $HOME/.kube
@@ -26,3 +29,20 @@ kubectl get pods -A
 
 echo "Please wait a few minutes to get all pods running before joining any worker nodes."
 
+
+# kubeadm config images pull
+
+## Install Calico   
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/calico.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/tigera-operator.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/custom-resources.yaml
+
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.4/manifests/tigera-operator.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.4/manifests/custom-resources.yaml
+
+#Wait for all the pods to run with STATUS of Running
+#watch kubectl get pods -n calico-system
+
+kubectl get pods -n kube-system
+kubectl get nodes -o wide
+kubectl get pods --all-namespaces -o wide
